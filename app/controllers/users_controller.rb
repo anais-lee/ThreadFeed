8567702da6bf082507b2e1ff7f07f39bf2126486
Angela <lee.anais@gmail.com>
2016-11-30 12:15:53 -0500

    signup completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
-  def new
-  end
+	def new
+		@user = User.new
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def create #called when form is submitted on signup page
+		@user = User.new(user_params)  #not in the final impl
+		if @user.save
+			flash[:success] = "Welcome to ThreadFeed!"
+      		redirect_to @user
+  		else
+  			render 'new'
+  		end
+	end
+
+	private
+		def user_params
+	      params.require(:user).permit(:name, :email, :password,
+	                                   :password_confirmation)
+	    end
 end
