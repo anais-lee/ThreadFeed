0418fef51f83236e51fc8c9bcb1ecae53da3ade3
Angela <lee.anais@gmail.com>
2016-11-30 15:26:26 -0500

    remember me done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
     	# Log the user in and redirect to the user's show page.
     	log_in user
-      	redirect_to user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_to user
     else
 
       flash.now[:danger] = 'Invalid email/password combination' 
@@ -15,9 +16,9 @@ class SessionsController < ApplicationController
     end
   end
 
-  def destroy 
-  	log_out
-  	redirect_to root_url
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
 
 
