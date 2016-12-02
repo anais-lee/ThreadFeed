de983c584cad3f6f43118aaba95881d6d152377a
Angela <lee.anais@gmail.com>
2016-12-02 14:38:49 -0500

    finished user edits, update, index, and destroy

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersLoginTest < ActionDispatch::IntegrationTest
 	def setup
     	@user = users(:angela)
+    	@other_user = users(:archer)
   	end
 
   	test "login with invalid information" do
@@ -36,4 +37,36 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 	    assert_select "a[href=?]", logout_path,      count: 0
 	    assert_select "a[href=?]", user_path(@user), count: 0
 	  end
+
+	test "should redirect edit when logged in as wrong user" do
+	    log_in_as(@other_user)
+	    get edit_user_path(@user)
+	    assert flash.empty?
+	    assert_redirected_to root_url
+	end
+
+	test "should redirect update when logged in as wrong user" do
+	    log_in_as(@other_user)
+	    patch user_path(@user), params: { user: { name: @user.name,
+	                                              email: @user.email } }
+	    assert flash.empty?
+	    assert_redirected_to root_url
+	end
+
+	test "successful edit with friendly forwarding" do
+	    get edit_user_path(@user)
+	    log_in_as(@user)
+	    assert_redirected_to edit_user_url(@user)
+	    name  = "Foo Bar"
+	    email = "foo@bar.com"
+	    patch user_path(@user), params: { user: { name:  name,
+	                                              email: email,
+	                                              password:              "",
+	                                              password_confirmation: "" } }
+	    assert_not flash.empty?
+	    assert_redirected_to @user
+	    @user.reload
+	    assert_equal name,  @user.name
+	    assert_equal email, @user.email
+	end
 end
